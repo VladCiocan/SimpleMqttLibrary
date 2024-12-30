376e09dcf3288287dfb0941ed45a1bea586d8f0f
Vlad Ciocan <vciocan@corprms.com>
2024-12-30 15:42:08 +0200

    fix

--- base64.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BASE__H_
-#define __BASE__H_
-
-int Base64decode_len(const char *bufcoded);
-int Base64decode(char *bufplain, const char *bufcoded);
-int Base64encode_len(int len);
-int Base64encode(char *encoded, const char *string, int len);
-
-#endif
